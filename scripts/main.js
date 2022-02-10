1c7283d0ac1d7f97fc05fc1f2b0664a25a114ae9
Dre P Harm <2008oxfordcomma@gmail.com>
2022-02-10 10:35:38 -0600

    Added the addClickHandler to delete checkbox

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,31 @@
 (function (window) {
     'use strict';
 
-    // let's make sure we only have one of each of these:
-
     const FORM_SELECTOR = '[data-coffee-order="form"]';
+    const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+
+    // let's make sure we only have one of each of these:
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
     let FormHandler = App.FormHandler;
+    let CheckList = App.CheckList;
 
     let myTruck = new Truck('12345', new DataStore());
-
     window.myTruck = myTruck;
 
+    // find the form that is being submitted and create a FormHandler object
     let formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-    console.log(formHandler);
-    
+    // find the checklist that is being updated and create a CheckList object
+    let checkList = new CheckList(CHECKLIST_SELECTOR);
+
+    // when a checkbox is clicked, call "deliverOrder" on myTruck
+    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+
+    // when the submit button is called, create an order and add a checkbox
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    })
+
 })(window);
