2fa6062056ea84731447f81f7240fa91cb85baec
Dre P Harm <2008oxfordcomma@gmail.com>
2022-02-16 10:16:43 -0600

    Added remove function

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -10,10 +10,34 @@
     }
 
     RemoteDataStore.prototype.add = function (key, val) {
-        // Call jQuery's $.post mathod to send the value to the serverUrl
         $.post(this.serverUrl, val, function (serverResponse) {
             console.log(serverResponse);
         });
+    };
+
+    RemoteDataStore.prototype.getAll = function (cb) {
+        // make a "get" call to the server URL
+        // pass in an anonymous function that calls the "cb" callback function
+        $.get(this.serverUrl, function (serverResponse) {
+            console.log(serverResponse);
+            cb(serverResponse);
+        });
+    };
+
+    RemoteDataStore.prototype.get = function (key, cb) {
+        // make a get call to the server but pass an email address
+        // so that it returns just one order
+        // then call the function "cb" on the response
+        $.get(this.serverUrl + '/' + key, function (serverResponse) {
+            console.log(serverResponse);
+            cb(serverResponse);
+        });
+    };
+
+    RemoteDataStore.prototype.remove = function (key) {
+        // call the server Url using the Ajax 'DELETE' command
+        $.ajax(this.serverUrl + '/' + key, {
+            type: 'DELETE' });
     };
 
     App.RemoteDataStore = RemoteDataStore;
